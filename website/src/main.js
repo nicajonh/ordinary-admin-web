c78cadf696394bf646d40b51164d0f30f309ce22
llh <leelinhong@outlook.com>
2020-04-18 11:42:08 +0800

    引入静态资源 设定简单的全局样式

--- website/src/main.js
+++ website/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import './assets/style/main.css'
+import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
