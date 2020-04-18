25c7d6e1a4df5afadefad5806525163a22a3cbe6
llh <leelinhong@outlook.com>
2020-04-18 11:48:02 +0800

    element-ui的css
    之前忘了加了

--- website/src/main.js
+++ website/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import './assets/style/main.css'
 import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 Vue.config.productionTip = false
