cfcb7398c2576c2d4c5c3e017decfab279cd4236
llh <leelinhong@outlook.com>
2020-04-20 21:31:54 +0800

    添加 路由导航守卫

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,35 +1,47 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import { getAccessToken } from '@/util/auth'
+Vue.use(VueRouter)
 
 const routes = [
     {
-        path: "/",
-        name: "Home",
+        path: '/',
+        name: 'Home',
         component: Home
     },
     {
-        path: "/login",
-        name: "login",
-        component: () => import("@/views/Login.vue")
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/Login.vue')
     },
     {
-        path: "/about",
-        name: "About",
+        path: '/about',
+        name: 'About',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-            import(/* webpackChunkName: "about" */ "../views/About.vue")
+            import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
-];
+]
 
 const router = new VueRouter({
-    mode: "history",
+    mode: 'history',
     base: process.env.BASE_URL,
     routes
-});
+})
+//
+/**
+ * 挂载路由导航守卫
+ * to 将要访问的路径
+ * from 来自于哪个路径
+ * next 函数对象。表示放行（到哪个路径）
+ */
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') return next()
+    if (!getAccessToken()) return next('/login')
+    next()
+})
 
-export default router;
+export default router
