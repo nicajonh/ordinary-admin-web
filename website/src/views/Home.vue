a19c23f8d9ed55d772504f9df043b43b49de4c3b
llh <leelinhong@outlook.com>
2020-04-22 21:40:53 +0800

    nav-top 部分的配置

--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <el-container class="home-container">
         <el-header>
-            Header
+            <div>
+                <el-avatar :size="50" :src="circleUrl"></el-avatar>
+                <span>管理系统</span>
+            </div>
             <el-button type="info" @click="logout">logout</el-button>
         </el-header>
         <el-container>
@@ -16,6 +19,12 @@ import { removeToken } from '@/util/auth'
 
 export default {
     name: 'Home',
+    data() {
+        return {
+            circleUrl:
+                'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
+        }
+    },
     methods: {
         logout() {
             removeToken()
@@ -27,6 +36,19 @@ export default {
 <style lang="less" scoped>
 .el-header {
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    > div {
+        display: flex;
+        align-items: center;
+        > span {
+            margin-left: 15px;
+        }
+    }
 }
 .el-aside {
     background-color: #333744;
