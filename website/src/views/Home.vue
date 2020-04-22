b0e9512b142282cce945d417a136158dd72a03ee
llh <leelinhong@outlook.com>
2020-04-22 21:10:23 +0800

    布局配置

--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="home">
-        <el-button type="info" @click="logout">logout</el-button>
-    </div>
+    <el-container class="home-container">
+        <el-header>
+            Header
+            <el-button type="info" @click="logout">logout</el-button>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -17,3 +24,17 @@ export default {
     }
 }
 </script>
+<style lang="less" scoped>
+.el-header {
+    background-color: #373d41;
+}
+.el-aside {
+    background-color: #333744;
+}
+.el-main {
+    background-color: #eaedf1;
+}
+.home-container {
+    height: 100%;
+}
+</style>
