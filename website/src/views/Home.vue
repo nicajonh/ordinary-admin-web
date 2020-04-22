dce4d950408848c049af3a9e3cde0f932d8753c9
llh <leelinhong@outlook.com>
2020-04-22 20:50:13 +0800

    简单的登出功能

--- website/src/views/Home.vue
+++ website/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <el-button type="info" @click="logout">logout</el-button>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { removeToken } from '@/util/auth'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    methods: {
+        logout() {
+            removeToken()
+            this.$router.push('/login')
+        }
+    }
 }
 </script>
