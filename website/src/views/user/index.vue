aaeafde02d50b094cc4f5fa9026adada0642fa89
llh <leelinhong@outlook.com>
2020-05-21 23:23:26 +0800

    用户列表画面部分样式

--- website/src/views/user/index.vue
+++ website/src/views/user/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      user page
-  </div>
+    <div>
+        <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <el-card >
+            <el-row :gutter="15">
+                <el-col :span="6" :xl="4">
+                    <el-input placeholder="请输入内容">
+                        <el-button
+                            slot="append"
+                            icon="el-icon-search"
+                        ></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="2">
+                  <el-button type="primary">添加用户</el-button>
+                </el-col>
+            </el-row>
+        </el-card>
+    </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
