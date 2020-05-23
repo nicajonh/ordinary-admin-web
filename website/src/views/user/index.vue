c8ac85ef838a785461b558ba028d1992d1c268df
llh <leelinhong@outlook.com>
2020-05-23 15:23:22 +0800

    加入 分页组件
    有待测试

--- website/src/views/user/index.vue
+++ website/src/views/user/index.vue
@@ -22,15 +22,67 @@
                 </el-col>
             </el-row>
 
-            <el-table :data="pageObj.list" style="width: 100%" border >
+            <el-table :data="pageObj.list" style="width: 100%" border>
+                <el-table-column type="index"></el-table-column>
                 <el-table-column
                     prop="username"
                     label="用户名"
                     width="180"
                 ></el-table-column>
 
-                <el-table-column label="操作" width="180"></el-table-column>
+                <el-table-column prop="accountStatus" label="状态" width="180">
+                    <template v-slot="scope">
+                        <el-tag v-if="scope.row.accountStatus == 0" type="info">
+                            未激活
+                        </el-tag>
+                        <el-tag
+                            v-else-if="scope.row.accountStatus == 1"
+                            type="success"
+                        >
+                            激活
+                        </el-tag>
+                        <el-tag
+                            v-else-if="scope.row.accountStatus == 2"
+                            type="warning"
+                        >
+                            锁定
+                        </el-tag>
+                        <el-tag
+                            v-else-if="scope.row.accountStatus == 3"
+                            type="danger"
+                        >
+                            删除
+                        </el-tag>
+                        <el-tag v-else type="danger">未知状态</el-tag>
+                    </template>
+                </el-table-column>
+
+                <el-table-column label="操作" width="150px">
+                    <template>
+                        <el-button
+                            type="primary"
+                            icon="el-icon-edit"
+                            size="mini"
+                        ></el-button>
+
+                        <el-button
+                            size="mini"
+                            type="danger"
+                            icon="el-icon-delete-solid"
+                        ></el-button>
+                    </template>
+                </el-table-column>
             </el-table>
+
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="queryInfo.pageNumber"
+                :page-sizes="[2, 5, 10, 50]"
+                :page-size="queryInfo.pageSize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="pageObj.totalEle"
+            ></el-pagination>
         </el-card>
     </div>
 </template>
@@ -61,6 +113,14 @@ export default {
                 this.pageObj.totalPage = resp.data.totalPages
                 this.pageObj.totalEle = resp.data.totalElements
             })
+        },
+        handleSizeChange(newSize) {
+            this.queryInfo.pageSize = newSize
+            this.getUserList()
+        },
+        handleCurrentChange(curPage) {
+            this.queryInfo.pageNumber = curPage
+            this.getUserList()
         }
     },
     created() {
