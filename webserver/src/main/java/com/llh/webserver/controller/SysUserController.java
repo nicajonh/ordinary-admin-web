0a39167ec8e500b7b4cb7582d3151ae58ad34767
llh <leelinhong@outlook.com>
2020-05-30 15:08:27 +0800

    添加 API注释

--- webserver/src/main/java/com/llh/webserver/controller/SysUserController.java
+++ webserver/src/main/java/com/llh/webserver/controller/SysUserController.java
@@ -6,6 +6,8 @@ import com.llh.webserver.pojo.SimplePageQueryVO;
 import com.llh.webserver.pojo.auth.RegisterOrUpdateVO;
 import com.llh.webserver.service.AuthService;
 import com.llh.webserver.service.SysUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("user")
+@Api("用户信息操作模块。")
 public class SysUserController extends BasicController {
 
     @Autowired
@@ -30,24 +33,28 @@ public class SysUserController extends BasicController {
     private AuthService authService;
 
     @GetMapping
+    @ApiOperation("获取用户列表，并以分页的形式返回。")
     public JsonWrapper page(SimplePageQueryVO<SysUser> pageQueryVO) {
         Page<SysUser> page = userService.pageQuery(pageQueryVO);
         return JsonWrapper.ok(page);
     }
 
     @PostMapping
+    @ApiOperation("新增一个用户。")
     public JsonWrapper add(@RequestBody RegisterOrUpdateVO user) {
         RegisterOrUpdateVO register = authService.register(user);
         return JsonWrapper.ok(register);
     }
 
     @GetMapping("{userId}")
+    @ApiOperation("根据用户id获取用户信息。")
     public JsonWrapper userInfo(@PathVariable("userId") String userId) {
         SysUser user = userService.findById(userId);
         return JsonWrapper.ok(user);
     }
 
     @PostMapping("update")
+    @ApiOperation("更新用户信息。")
     public JsonWrapper update(@RequestBody SysUser user) {
         userService.updateById(user);
         return JsonWrapper.ok(user);
