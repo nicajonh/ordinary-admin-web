9a50de3b6416e2b9e5d536fc5db39f26a3a2bad0
llh <leelinhong@outlook.com>
2020-06-08 23:11:45 +0800

    修改 文件夹名

--- webserver/src/main/java/com/llh/webserver/util/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * package-info
- * <p>
- * CreatedAt: 2020-04-18 21:59
- *
- * @author llh
- */
-package com.llh.webserver.util;
--- website/src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import request from '@/util/request'
-
-export function login(data) {
-    return request({
-        url: '/auth/login',
-        method: 'post',
-        data
-    })
-}
