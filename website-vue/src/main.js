d4ef4fa5ab30ac7d2350a35f1fb749079ba4e7da
llh <leelinhong@outlook.com>
2020-06-12 00:13:23 +0800

    移除 font-awesome源码 使用其npm依赖

--- website-vue/src/main.js
+++ website-vue/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/style/main.css'
-import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
+import 'font-awesome/css/font-awesome.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
