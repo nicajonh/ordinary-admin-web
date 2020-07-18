0b854872a6c7a3b1d37f2b34de2ccf8af3f1c549
llh <leelinhong@outlook.com>
2020-07-19 00:46:14 +0800

    优化 配置方案

--- server4kt/src/main/kotlin/com/llh/server/common/config/CodeGenProperties.kt
+++ server4kt/src/main/kotlin/com/llh/server/common/config/CodeGenProperties.kt
@@ -1,6 +1,10 @@
 package com.llh.server.common.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.env.YamlPropertySourceLoader
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.io.support.DefaultPropertySourceFactory
+import org.springframework.core.io.support.EncodedResource
 import org.springframework.stereotype.Component
 
 /**
@@ -10,12 +14,25 @@ import org.springframework.stereotype.Component
  *
  * @author llh
  */
+@PropertySource(value = ["classpath:code-gen.yml"], factory = ResponseFactory::class)
 @Component
 @ConfigurationProperties(prefix = "code-gen-properties")
 class CodeGenProperties {
 
     var typeMap: Map<String, String> = mutableMapOf()
+    var typeMapJs: Map<String, String> = mutableMapOf()
     var auth: String? = null
     var tablePrefix: String? = null
     var ignoreFields: String? = null
+
+}
+
+/** 支持yml文件读取 */
+class ResponseFactory : DefaultPropertySourceFactory() {
+
+    override fun createPropertySource(name: String?, resource: EncodedResource): org.springframework.core.env.PropertySource<*> {
+        if (resource == null) // 调用java代码，真有可能是null
+            return super.createPropertySource(name, resource)
+        return YamlPropertySourceLoader().load(resource.resource.filename, resource.resource)[0]
+    }
 }
