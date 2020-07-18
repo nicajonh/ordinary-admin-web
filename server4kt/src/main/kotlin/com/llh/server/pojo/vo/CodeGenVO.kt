687d01e112bc76a33df414f1409d90bf62b2d614
llh <leelinhong@outlook.com>
2020-07-18 11:22:29 +0800

    API模型注释

--- server4kt/src/main/kotlin/com/llh/server/pojo/vo/CodeGenVO.kt
+++ server4kt/src/main/kotlin/com/llh/server/pojo/vo/CodeGenVO.kt
@@ -7,10 +7,17 @@
  */
 package com.llh.server.pojo.vo
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 
+@ApiModel("代码生成配置。")
 data class CodeGenVO(
+    @ApiModelProperty("模块简述")
     val modelDescription: String,
+    @ApiModelProperty("url前缀")
     val urlPrefix: String,
+    @ApiModelProperty("数据库表名")
     val tableName: String,
+    @ApiModelProperty("java包名")
     val javaPackage: String?
 )
